f53a7bb71bf0bcdf02a139588f2ab4e87b59e8c4
Pushpal Ghoshal <pushpal.ghoshal@rediffmail.com>
2021-05-24 22:17:22 +0530

    added listen and port config again

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const client = new Stonks();
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
 
 client.login(process.env.TOKEN);
