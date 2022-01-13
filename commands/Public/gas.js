bb2ae75ba98fadc00097e1f6e4365af8373a3b07
Pushpal Ghoshal <1804252@kiit.ac.in>
2022-01-13 19:49:46 +0530

    gas command fix 2

--- commands/Public/gas.js
+++ commands/Public/gas.js
@@ -78,7 +78,6 @@ class GasCommand extends Command {
         console.log(chalk.green("Etherscan gas requested by " + chalk.yellow(message.author.username)));
         axios.get('https://etherscan.io/gastracker', {
             headers: {
-                'Content-Type': 'application/json',
                 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
             }
         })
@@ -123,7 +122,7 @@ class GasCommand extends Command {
                 }
             }).catch((error) => {
                 console.log(error)
-                console.error({ error });
+            
             });
 
 
